a5186cc886e9281fb7b6f58b107fd4b9387bf21b
koss054 <konstantin054@abv.bg>
2026-07-13 23:27:28 +0300

    add custom icons class and drawer logo

---
+++ MudBarber.Web/Theme/CustomIcons.cs
@@ -0,0 +1,15 @@
+namespace MudBarber.Web.Theme;
+
+public static class CustomIcons
+{
+    public const string Scissors =
+        """
+        <g fill="none" stroke="currentColor" stroke-width="2"
+           stroke-linecap="round" stroke-linejoin="round">
+          <path d="M3 7a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+          <path d="M3 17a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+          <path d="M8.6 8.6l10.4 10.4" />
+          <path d="M8.6 15.4l10.4 -10.4" />
+        </g>
+        """;
+}
