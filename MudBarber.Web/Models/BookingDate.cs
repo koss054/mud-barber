51cbc1e61a7744a077377ef74ed3b09effc1e450
koss054 <konstantin054@abv.bg>
2026-07-26 22:53:38 +0300

    rework: remove BookingDate and BookingTime

--- MudBarber.Web/Models/BookingDate.cs
+++
@@ -1,8 +0,0 @@
-namespace MudBarber.Web.Models;
-
-public class BookingDate
-{
-    public DateOnly Date { get; set; }
-    public bool Available { get; set; }
-    public IEnumerable<BookingTime> Time { get; set; } = [];
-}
--- MudBarber.Web/Models/BookingTime.cs
+++
@@ -1,8 +0,0 @@
-namespace MudBarber.Web.Models;
-
-public class BookingTime
-{
-    public TimeOnly Time { get; set; }
-    public bool Available { get; set; }
-    public bool Selected { get; set; }
-}
