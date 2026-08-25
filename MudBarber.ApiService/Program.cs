4912b5ebf3f8dfac17ed5a169fbbd5388bea845d
koss054 <konstantin054@abv.bg>
2026-08-25 23:23:51 +0300

    feat: map booking endpoints

--- MudBarber.ApiService/Program.cs
+++ MudBarber.ApiService/Program.cs
@@ -1,8 +1,14 @@
+using MudBarber.ApiService.Data;
+using MudBarber.ApiService.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+
 builder.Services.AddProblemDetails();
 builder.Services.AddOpenApi();
+
+builder.AddNpgsqlDbContext<MudBarberDbContext>("postgresdb");
 
 var app = builder.Build();
 
@@ -13,5 +19,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.MapBookingEndpoints();
 app.MapDefaultEndpoints();
+
 app.Run();
