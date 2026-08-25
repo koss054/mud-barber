17605d8894ab0c6e32fd3b942a8cc83ec95c242e
koss054 <konstantin054@abv.bg>
2026-08-25 22:24:14 +0300

    chore: remove weather api client

--- MudBarber.Web/Program.cs
+++ MudBarber.Web/Program.cs
@@ -1,4 +1,3 @@
-using MudBarber.Web;
 using MudBarber.Web.Components;
 using MudBlazor.Services;
 
@@ -12,12 +11,12 @@ builder.Services.AddRazorComponents()
 builder.Services.AddOutputCache();
 
 
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-{
-    // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-    // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-    client.BaseAddress = new("https+http://apiservice");
-});
+// builder.Services.AddHttpClient<WeatherApiClient>(client =>
+// {
+//     // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
+//     // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
+//     client.BaseAddress = new("https+http://apiservice");
+// });
 
 builder.Services.AddMudServices();
 
