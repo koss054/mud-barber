5fb426ff8d6b525590b548afa8a5afdfd912779a
koss054 <konstantin054@abv.bg>
2026-08-25 23:36:49 +0300

    feat: migrations on api service run

--- MudBarber.ApiService/Program.cs
+++ MudBarber.ApiService/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using MudBarber.ApiService.Data;
 using MudBarber.ApiService.Endpoints;
 
@@ -11,6 +12,12 @@ builder.Services.AddOpenApi();
 builder.AddNpgsqlDbContext<MudBarberDbContext>("postgresdb");
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<MudBarberDbContext>();
+    await db.Database.MigrateAsync();
+}
 
 app.UseExceptionHandler();
 
