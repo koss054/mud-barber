a6863885e16642f64686ca8ba9d72293da0764c4
koss054 <konstantin054@abv.bg>
2026-08-26 20:00:03 +0300

    rework: barber endpoints

--- MudBarber.ApiService/Endpoints/BarberEndpoints.cs
+++ MudBarber.ApiService/Endpoints/BarberEndpoints.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using MudBarber.ApiService.Data;
-using MudBarber.ApiService.Dtos;
+using MudBarber.ApiService.Dtos.Barbers;
+using MudBarber.ApiService.Mapping;
 
 namespace MudBarber.ApiService.Endpoints;
 
@@ -11,9 +12,21 @@ public static class BarberEndpoints
     {
         var group = app.MapGroup("/barbers").WithTags("Barbers");
 
+        group.MapPost("/", Create);
         group.MapGet("/", GetAll);
 
         return group;
+    }
+
+    private static async Task<Created<BarberDto>> Create(
+        CreateBarberRequest request, MudBarberDbContext db, CancellationToken ct)
+    {
+        var barber = request.ToEntity();
+
+        db.Barbers.Add(barber);
+        await db.SaveChangesAsync(ct);
+
+        return TypedResults.Created($"/barbers/{barber.Id}", barber.ToDto());
     }
 
     private static async Task<Ok<List<BarberDto>>> GetAll(
@@ -21,16 +34,8 @@ public static class BarberEndpoints
     {
         var barbers = await db.Barbers
             .AsNoTracking()
-            .Select(b => new BarberDto
-            {
-                Id = b.Id,
-                FirstName = b.FirstName,
-                LastName = b.LastName,
-                Rating = b.Rating,
-                IsActive = b.IsActive
-            })
             .ToListAsync(ct);
 
-        return TypedResults.Ok(barbers);
+        return TypedResults.Ok(barbers.Select(b => b.ToDto()).ToList());
     }
 }
