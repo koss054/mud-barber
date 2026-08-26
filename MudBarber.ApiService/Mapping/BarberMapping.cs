40e334bbe7f239568a6cfcdc6720c8fcec5977e1
koss054 <konstantin054@abv.bg>
2026-08-26 19:59:49 +0300

    feat: barber mapping extension

---
+++ MudBarber.ApiService/Mapping/BarberMapping.cs
@@ -0,0 +1,26 @@
+using MudBarber.ApiService.Data.Entities;
+using MudBarber.ApiService.Dtos.Barbers;
+
+namespace MudBarber.ApiService.Mapping;
+
+public static class BarberMapping
+{
+    public static BarberDto ToDto(this Barber barber) =>
+        new()
+        {
+            Id = barber.Id,
+            FirstName = barber.FirstName,
+            LastName = barber.LastName,
+            Rating = barber.Rating,
+            IsActive = barber.IsActive
+        };
+
+    public static Barber ToEntity(this CreateBarberRequest request) =>
+        new()
+        {
+            Id = Guid.CreateVersion7(),
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            IsActive = true
+        };
+}
