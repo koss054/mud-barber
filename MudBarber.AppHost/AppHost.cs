ac8532910f3095c0c664060ee10f8e0721757c48
koss054 <konstantin054@abv.bg>
2026-09-10 21:28:56 +0300

    feat: azure deployment init

--- MudBarber.AppHost/AppHost.cs
+++ MudBarber.AppHost/AppHost.cs
@@ -1,10 +1,22 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+/*
+TODO: adjust for local development so I don't need to uncomment this locally
 var postgres = builder.AddPostgres("postgres")
     .WithDataVolume(isReadOnly: false)
     .WithPgWeb();
 
 var postgresdb = postgres.AddDatabase("postgresdb");
+*/
+
+var postgres = builder
+    .AddAzurePostgresFlexibleServer("postgres")
+    .RunAsContainer();
+
+var postgresdb = postgres.AddDatabase("postgresdb");
+
+// TODO: finish setting this up to test AppInsights in Azure
+// var appInsightsConnString = builder.Configuration["AppInsightsConnString"];
 
 var apiService = builder.AddProject<Projects.MudBarber_ApiService>("apiservice")
     .WithHttpHealthCheck("/health")
