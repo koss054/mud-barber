46613863c3b661e241deb177bb5555911a708e2d
koss054 <konstantin054@abv.bg>
2026-09-22 20:32:04 +0300

    feat: step labels initial style

--- MudBarber.Web/Components/Shared/Stepper/StepState.cs
+++ MudBarber.Web/Components/Shared/Stepper/StepState.cs
@@ -1,11 +1,9 @@
 namespace MudBarber.Web.Components.Shared.Stepper;
 
-/// <summary>
-/// Completion/availability of a stepper step. 
-/// </summary>
 public enum StepState
 {
     Locked,
     Available,
+    Active,
     Completed
 }
